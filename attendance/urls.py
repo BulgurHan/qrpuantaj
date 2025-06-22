c56147f2e750506ec2f55688590e169cac1dcaab
BulgurHan <mertgurkan034@gmail.com>
2025-06-22 12:45:26 +0300

    endpoint urli yanlis bverilmis duzeltildi

--- attendance/urls.py
+++ attendance/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import scan_qr, CompanyInfoView
 
 urlpatterns = [
-    path('scan_qr/', scan_qr, name='scan_qr'),
+    path('scan-qr/', scan_qr, name='scan_qr'),
     path('company-info/', CompanyInfoView.as_view()),
 ]
