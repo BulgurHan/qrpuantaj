14258683d3a2e48cb6cccfc320c882e5ad35ee4f
BulgurHan <mertgurkan034@gmail.com>
2025-06-23 12:28:21 +0300

    debug islemleri

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from datetime import timedelta
-from users.models import User  # Kullanıcı modelini içe aktar
+from users.models import User  
 from django.contrib.auth import get_user_model
 import uuid
 
